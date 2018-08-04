3b87cacae21b1906898d782bf64192d2f01cfb10
Gator96100 <jupuerzi@gmx.at>
2018-08-04 13:33:16 +0200

    Fixed missing qwindows.dll

--- msys2/autobuild.sh
+++ msys2/autobuild.sh
@@ -4,7 +4,6 @@ copyDir=/builds
 buildDir=/tmp
 
 export PATH=$PATH:/gcc-arm-none-eabi/bin
-export QT_MSYS2_STATIC=true
 
 pacman -Q zip 1> /dev/null
 if [[ $? != 0 ]]; then
@@ -14,19 +13,11 @@ fi
 if [ "$MSYSTEM" == "MINGW32" ]; then
   mingwDir=/mingw32
   arch=32
-  pacman -Q mingw-w64-i686-qt5-static 1> /dev/null
-  if [[ $? != 0 ]]; then
-    pacman --noconfirm -S mingw-w64-i686-qt5-static
-  fi
 fi
 
 if [ "$MSYSTEM" == "MINGW64" ]; then
   mingwDir=/mingw64
   arch=64
-  pacman -Q mingw-w64-x86_64-qt5-static 1> /dev/null
-  if [[ $? != 0 ]]; then
-    pacman --noconfirm -S mingw-w64-x86_64-qt5-static
-  fi
 fi
 
 cd $pm3Dir
@@ -37,13 +28,13 @@ for i in $( ls ); do
   git pull
   hash=$(git rev-parse HEAD)
   date=$(date +%Y%m%d)
-  if ! ls $copyDir/$i-$arch/*-$hash.zip 1> /dev/null 2>&1; then
+ if ! ls $copyDir/$i-$arch/*-$hash.zip 1> /dev/null 2>&1; then
     make clean
 	make all
 	if [ $? -eq 0 ]; then
 	  rm -rf $buildDir/$i/*
-	  mkdir -p $copyDir/$i-$arch
-	  mkdir -p $buildDir/$i/win$arch
+	  mkdir -p $copyDir/$i-$arch/
+	  mkdir -p $buildDir/$i/win$arch/platforms
 	  mkdir -p $buildDir/$i/firmware_win/bootrom
 	  mkdir -p "$buildDir/$i/firmware_win/JTAG Only"
 	  mkdir -p "$buildDir/$i/Windows Driver"
@@ -51,6 +42,7 @@ for i in $( ls ); do
 	  cp $mingwDir/bin/{libbz2-1.dll,libfreetype-6.dll,libgcc_s_dw2-1.dll,libglib-2.0-0.dll,libgraphite2.dll,libharfbuzz-0.dll,libiconv-2.dll,libicudt61.dll,libicuin61.dll,libicuuc61.dll,libintl-8.dll,libpcre-1.dll,libpcre2-16-0.dll,libpng16-16.dll,libreadline7.dll,libstdc++-6.dll,libtermcap-0.dll,libwinpthread-1.dll,Qt5Core.dll,Qt5Gui.dll,Qt5Widgets.dll,zlib1.dll,libgcc_s_seh-1.dll} $buildDir/$i/win$arch
 	  cp -r /autobuild/* $buildDir/$i/win$arch
 	  cp -r $pm3Dir/$i/client/{proxmark3.exe,flasher.exe,*.dic,lualibs,scripts,hardnested} $buildDir/$i/win$arch
+	  cp $mingwDir/share/qt5/plugins/platforms/qwindows.dll $buildDir/$i/win$arch/platforms
 	  cp $pm3Dir/$i/armsrc/obj/{fullimage.elf,fullimage.s19} $buildDir/$i/firmware_win
 	  cp $pm3Dir/$i/bootrom/obj/{bootrom.elf,bootrom.s19} $buildDir/$i/firmware_win/bootrom
 	  cp $pm3Dir/$i/recovery/{bootrom.bin,fullimage.bin,proxmark3_recovery.bin} "$buildDir/$i/firmware_win/JTAG Only"

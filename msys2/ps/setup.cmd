70dad0600017c06371d5a71acb4d8c8bda8f46e6
gator96100 <jupuerzi@gmx.at>
2020-01-31 23:20:34 +0100

    Fixed wrong fstab entries

--- msys2/ps/setup.cmd
+++ msys2/ps/setup.cmd
@@ -6,8 +6,8 @@ copy /Y %cd%\msys2\ps\*.conf %cd%\msys2\etc\ >NUL
 echo # For a description of the file format, see the Users Guide >%cd%\msys2/etc/fstab 
 echo # http://cygwin.com/cygwin-ug-net/using.html#mount-table >>%cd%\msys2/etc/fstab 
 echo none / cygdrive binary,posix=0,noacl,user 0 0 >>%cd%\msys2/etc/fstab
-echo %~dp0pm3 /pm3 ntfs noacl 0 0 >>%cd%\msys2/etc/fstab
-echo %~dp0gcc-arm-none-eabi /gcc-arm-none-eabi ntfs noacl 0 0 >>%cd%\msys2/etc/fstab
+echo %cd%\pm3 /pm3 ntfs noacl 0 0 >>%cd%\msys2/etc/fstab
+echo %cd%\gcc-arm-none-eabi /gcc-arm-none-eabi ntfs noacl 0 0 >>%cd%\msys2/etc/fstab
 
 dash /usr/bin/rebaseall -p 2> nul
 mkdir %cd%\msys2\tmp 2> nul

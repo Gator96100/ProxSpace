b66ceaed9894fef147b37993e5a4b7a0b58055a6
gator96100 <jupuerzi@gmx.at>
2020-01-31 21:02:26 +0100

    Added ps-info and ps-setup command

--- runme.bat
+++ runme.bat
@@ -1,12 +1,9 @@
 @echo off
-REM *******************************************
-REM CHANGE THIS PATH TO MATCH YOUR INSTALLATION
-REM *******************************************
-set MYPATH=%~dp0
+
 copy /Y msys2\ps\*.post msys2\etc\post-install\ >NUL
 echo # For a description of the file format, see the Users Guide >msys2/etc/fstab 
 echo # http://cygwin.com/cygwin-ug-net/using.html#mount-table >>msys2/etc/fstab 
 echo none / cygdrive binary,posix=0,noacl,user 0 0 >>msys2/etc/fstab
-echo %MYPATH%pm3 /pm3 ntfs noacl 0 0 >>msys2/etc/fstab
-echo %MYPATH%gcc-arm-none-eabi /gcc-arm-none-eabi ntfs noacl 0 0 >>msys2/etc/fstab
+echo %~dp0pm3 /pm3 ntfs noacl 0 0 >>msys2/etc/fstab
+echo %~dp0gcc-arm-none-eabi /gcc-arm-none-eabi ntfs noacl 0 0 >>msys2/etc/fstab
 msys2\msys2_shell.cmd -mingw32 -defterm -no-start

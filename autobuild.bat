a3183d3e21317326431a484e48854cf2ccc6555a
gator96100 <jupuerzi@gmx.at>
2020-02-01 00:03:45 +0100

    Fixed path for autobuild

--- autobuild.bat
+++ autobuild.bat
@@ -1,5 +1,5 @@
 @echo off
 call msys2\ps\setup.cmd
-echo %~dp0builds /builds ntfs noacl 0 0 >>msys2/etc/fstab
+echo %cd%\builds /builds ntfs noacl 0 0 >>msys2/etc/fstab
 call msys2\msys2_shell.cmd -mingw32 -defterm -no-start -c /ps/autobuild.sh
 call msys2\msys2_shell.cmd -mingw64 -defterm -no-start -c /ps/autobuild.sh

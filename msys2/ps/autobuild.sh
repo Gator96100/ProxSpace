9cf85b5f0d2ccca2018b5dd9eb336c132bfd48f9
Winds <60715874+Windslab@users.noreply.github.com>
2020-03-15 17:56:58 +0300

    Update autobuild.sh
    Fix 'scripts' to 'luascripts' folder.
    Add '*.bin,*.eml,*.html,*.json' formats to pack it to package.

--- msys2/ps/autobuild.sh
+++ msys2/ps/autobuild.sh
@@ -39,7 +39,7 @@ for i in $( ls -d */ ); do
 	  #you have to thank QT5 for all that dlls
 	  cp $mingwDir/bin/{libbz2-1.dll,libdouble-conversion.dll,libfreetype-*.dll,libgcc_s_*.dll,libglib-2.0-0.dll,libgraphite2.dll,libharfbuzz-0.dll,libiconv-2.dll,libicudt*.dll,libicuin*.dll,libicuuc*.dll,libintl-8.dll,libpcre-1.dll,libpcre2-16-0.dll,libpng16-16.dll,libreadline*.dll,libstdc++-*.dll,libtermcap-0.dll,libwinpthread-1.dll,Qt5Core.dll,Qt5Gui.dll,Qt5Widgets.dll,zlib1.dll,libsndfile-1.dll,libFLAC-8.dll,libogg-0.dll,libspeex-1.dll,libvorbis-0.dll,libvorbisenc-2.dll,libzstd.dll} $buildDir/${i%%/}/win$arch
 	  cp -r /ps/autobuild/* $buildDir/${i%%/}/win$arch
-	  cp -r $pm3Dir/${i%%/}/client/{proxmark3.exe,flasher.exe,*.dic,lualibs,scripts,hardnested} $buildDir/${i%%/}/win$arch
+	  cp -r $pm3Dir/${i%%/}/client/{proxmark3.exe,flasher.exe,*.dic,*.bin,*.eml,*.html,*.json,dictionaries,lualibs,luascripts,hardnested} $buildDir/${i%%/}/win$arch
 	  cp $mingwDir/share/qt5/plugins/platforms/qwindows.dll $buildDir/${i%%/}/win$arch/platforms
 	  cp $pm3Dir/${i%%/}/armsrc/obj/{fullimage.elf,fullimage.s19} $buildDir/${i%%/}/firmware_win
 	  cp $pm3Dir/${i%%/}/bootrom/obj/{bootrom.elf,bootrom.s19} $buildDir/${i%%/}/firmware_win/bootrom

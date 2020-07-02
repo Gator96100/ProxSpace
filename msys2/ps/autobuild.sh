33ee899c0b36bedc70bcfe6e898cd2cf4d5579c5
Gator96100 <jupuerzi@gmx.at>
2020-07-02 23:28:58 +0200

    Using multithreading

--- msys2/ps/autobuild.sh
+++ msys2/ps/autobuild.sh
@@ -28,7 +28,7 @@ for i in $( ls -d */ ); do
   date=$(date +%Y%m%d)
  if ! ls $copyDir/${i%%/}-$arch/*-$hash.7z 1> /dev/null 2>&1; then
     make clean
-	make all
+	make all -j$(nproc)
 	if [ $? -eq 0 ]; then
 	  rm -rf $buildDir/${i%%/}/*
 	  mkdir -p $copyDir/${i%%/}-$arch/

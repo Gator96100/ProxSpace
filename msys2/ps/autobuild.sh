05a31cbeb680cc691510cbc88e391cc79c019acf
Gator96100 <jupuerzi@gmx.at>
2020-08-04 21:50:19 +0200

    Added missing dlls

--- msys2/ps/autobuild.sh
+++ msys2/ps/autobuild.sh
@@ -37,7 +37,7 @@ for i in $( ls -d */ ); do
 	  mkdir -p "$buildDir/${i%%/}/firmware_win/JTAG Only"
 	  mkdir -p "$buildDir/${i%%/}/Windows Driver"
 	  #you have to thank QT5 for all that dlls
-	  cp $mingwDir/bin/{libbz2-1.dll,libdouble-conversion.dll,libfreetype-*.dll,libgcc_s_*.dll,libglib-2.0-0.dll,libgraphite2.dll,libharfbuzz-0.dll,libiconv-2.dll,libicudt*.dll,libicuin*.dll,libicuuc*.dll,libintl-8.dll,libpcre-1.dll,libpcre2-16-0.dll,libpng16-16.dll,libreadline*.dll,libstdc++-*.dll,libtermcap-0.dll,libwinpthread-1.dll,Qt5Core.dll,Qt5Gui.dll,Qt5Widgets.dll,zlib1.dll,libsndfile-1.dll,libFLAC-8.dll,libogg-0.dll,libspeex-1.dll,libvorbis-0.dll,libvorbisenc-2.dll,libzstd.dll,libpython3.8.dll,libjansson-4.dll} $buildDir/${i%%/}/win$arch
+	  cp $mingwDir/bin/{libbz2-1.dll,libdouble-conversion.dll,libfreetype-*.dll,libgcc_s_*.dll,libglib-2.0-0.dll,libgraphite2.dll,libharfbuzz-0.dll,libiconv-2.dll,libicudt*.dll,libicuin*.dll,libicuuc*.dll,libintl-8.dll,libpcre-1.dll,libpcre2-16-0.dll,libpng16-16.dll,libreadline*.dll,libstdc++-*.dll,libtermcap-0.dll,libwinpthread-1.dll,Qt5Core.dll,Qt5Gui.dll,Qt5Widgets.dll,zlib1.dll,libsndfile-1.dll,libFLAC-8.dll,libogg-0.dll,libspeex-1.dll,libvorbis-0.dll,libvorbisenc-2.dll,libzstd.dll,libpython3.8.dll,libjansson-4.dll,libbrotlidec.dll,libbrotlicommon.dll} $buildDir/${i%%/}/win$arch
 	  cp -r /ps/autobuild/* $buildDir/${i%%/}/win$arch
 	  cp -r $pm3Dir/${i%%/}/client/{proxmark3.exe,flasher.exe,*.dic,lualibs,scripts,pyscripts,luascripts,lualibs,hardnested,dictionaries,resources} $buildDir/${i%%/}/win$arch
 	  cp $mingwDir/share/qt5/plugins/platforms/qwindows.dll $buildDir/${i%%/}/win$arch/platforms

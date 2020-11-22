0e843ceb23322e8498e07721bde91970fb8d5130
Gator96100 <jupuerzi@gmx.at>
2020-11-22 22:04:14 +0100

    Automatically reopen ProxSpace after the initial msys2 update

--- runme64.bat
+++ runme64.bat
@@ -1,3 +1,4 @@
 @echo off
+cd %~dp0
 call msys2\ps\setup.cmd
-call msys2\msys2_shell.cmd -mingw64 -defterm -no-start %*
+call msys2\msys2_shell.cmd -mingw64 -defterm -no-start %*call msys2\msys2_shell.cmd -mingw64 -defterm -no-start %*if not exist msys2\ps\installed64.txt msys2\msys2_shell.cmd -mingw64 -defterm -no-start %*

16b4d205162d90f6ea3e4e6c576fe3c796b94eab
gator96100 <jupuerzi@gmx.at>
2021-01-18 20:26:09 +0100

    Fixed autobuild build folder not mounting correct after install

--- autobuild.bat
+++ autobuild.bat
@@ -1,4 +1,5 @@
 @echo off
 call msys2\ps\setup.cmd
-echo %cd%\builds /builds ntfs noacl 0 0 >>msys2/etc/fstab
+taskkill /IM "gpg-agent.exe" /F
+echo %~dp0builds /builds ntfs noacl 0 0 >>msys2/etc/fstab
 call msys2\msys2_shell.cmd -mingw64 -defterm -no-start -c /ps/autobuild.sh

486640dccc8c84574241b5a95ee9630155190a57
gator96100 <jupuerzi@gmx.at>
2021-01-18 20:39:47 +0100

    Additional info for forcing a COM port

--- msys2/ps/autobuild/rrg/pm3-flash-fullimage.bat
+++ msys2/ps/autobuild/rrg/pm3-flash-fullimage.bat
@@ -1,5 +1,7 @@
 @echo off
 cd client
 call setup.bat
+::If you want to force the COM port use the -p parameter, example:
+::bash pm3-flash-fullimage -p COM3
 bash pm3-flash-fullimage
 pause
--- msys2/ps/autobuild/rrg/pm3.bat
+++ msys2/ps/autobuild/rrg/pm3.bat
@@ -1,5 +1,7 @@
 @echo off
 cd client
 call setup.bat
+::If you want to force the COM port use the -p parameter, example:
+::bash pm3 -p COM3
 bash pm3
 pause

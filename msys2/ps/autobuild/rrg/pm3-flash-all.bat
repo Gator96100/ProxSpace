d5c364e5ec6c8a77aeb4bc689bdf6486ebdc8327
gator96100 <jupuerzi@gmx.at>
2021-02-10 02:01:44 +0100

    Fixed pm3-flash files

--- msys2/ps/autobuild/rrg/pm3-flash-all.bat
+++ msys2/ps/autobuild/rrg/pm3-flash-all.bat
@@ -1,7 +1,7 @@
 @echo off
 cd client
 call setup.bat
-::If you want to force the COM port use the -p parameter, example:
-::bash pm3-flash-all -p COM3
+::If you want to force the COM port add it to the line, example:
+::bash pm3-flash-all COM3
 bash pm3-flash-all
 pause
--- msys2/ps/autobuild/rrg/pm3-flash-bootrom.bat
+++ msys2/ps/autobuild/rrg/pm3-flash-bootrom.bat
@@ -1,7 +1,7 @@
 @echo off
 cd client
 call setup.bat
-::If you want to force the COM port use the -p parameter, example:
-::bash pm3-flash-bootrom -p COM3
+::If you want to force the COM port add it to the line, example:
+::bash pm3-flash-bootrom COM3
 bash pm3-flash-bootrom
 pause

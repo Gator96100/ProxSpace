d096c4086a798e46f902fb6d93ddea773fc9810b
Gator96100 <jupuerzi@gmx.at>
2021-04-11 00:58:03 +0200

    V3.9 (#44)
    Updated msys2 base to 20210228

--- setup/setup.cmd
+++ setup/setup.cmd
@@ -9,8 +9,12 @@ echo none / cygdrive binary,posix=0,noacl,user 0 0 >>%cd%\msys2/etc/fstab
 echo %cd%\setup /setup ntfs noacl 0 0 >>%cd%\msys2/etc/fstab
 echo %cd%\pm3 /pm3 ntfs noacl 0 0 >>%cd%\msys2/etc/fstab
 
-dash /usr/bin/rebaseall -p 2> nul
 mkdir %cd%\msys2\tmp 2> nul
+mkdir %cd%\msys2\dev 2> nul
+mkdir %cd%\msys2\otp 2> nul
+
+dash /usr/bin/rebaseall -p 2> nul
+
 del %cd%\msys2\etc\passwd 2> nul
 del %cd%\msys2\etc\group 2> nul
 touch /etc/passwd

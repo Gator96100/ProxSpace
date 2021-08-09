f9d8cb61ef3ebe6c668da38034f57c5a6b3b6e1f
Gator96100 <jupuerzi@gmx.at>
2021-08-09 23:36:12 +0200

    delete pm3 folder for release

--- setup/makerelease.bat
+++ setup/makerelease.bat
@@ -6,3 +6,4 @@ del /Q ..\msys2\etc\fstab
 del /Q ..\msys2\var\log
 move /Y ..\msys2\etc\pacman.d\mirrorlist.mingw64.backup ..\msys2\etc\pacman.d\mirrorlist.mingw64
 move /Y ..\msys2\etc\pacman.d\mirrorlist.msys.backup ..\msys2\etc\pacman.d\mirrorlist.msys
+rmdir /Q /S ..\pm3

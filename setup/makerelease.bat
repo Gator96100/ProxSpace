193d93c932af6d33d6dda8874c676cf0aec2cf34
Gator96100 <jupuerzi@gmx.at>
2021-08-09 23:12:00 +0200

    Restore mirror list for releases

--- setup/makerelease.bat
+++ setup/makerelease.bat
@@ -1,7 +1,8 @@
 @echo off
 cd %~dp0
-del /Q installed
 del /Q ..\msys2\etc\passwd
 del /Q ..\msys2\etc\group
 del /Q ..\msys2\etc\fstab
 del /Q ..\msys2\var\log
+move /Y ..\msys2\etc\pacman.d\mirrorlist.mingw64.backup ..\msys2\etc\pacman.d\mirrorlist.mingw64
+move /Y ..\msys2\etc\pacman.d\mirrorlist.msys.backup ..\msys2\etc\pacman.d\mirrorlist.msys
